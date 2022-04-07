03a755ef7a18df7ec5a70a9657e09e6aedf5aca9
royderks <10717410+royderks@users.noreply.github.com>
2022-04-07 15:21:06 +0200

    All materials ex-3

--- client.js
+++
@@ -1,8 +0,0 @@
-import { GraphQLClient } from 'graphql-request';
-
-const client = new GraphQLClient(
-  'https://graphqlbf.stepzen.net/api/bf215181b5140522137b3d4f6b73544a/__graphql',
-  { headers: {} },
-);
-
-export default client;
